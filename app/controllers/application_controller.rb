402a51468436c216464cfc7ccc9f8fe42ec0524f
Andrre Lima <andre.salatalima@gmail.com>
2024-06-10 17:08:02 +0200

    calls https for heroku limited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_https
+
+  def ensure_https
+    if request.headers['X-Forwarded-Proto'] != 'https'
+      redirect_to protocol: 'https://', status: :moved_permanently
+    end
+  end
+
 end
