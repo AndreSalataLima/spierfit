bc1e925e0714c9aa9d1635f57568d9383815677a
Andrre Lima <andre.salatalima@gmail.com>
2024-06-15 10:42:55 +0200

    included_feature_machine_model_view_controller

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,35 +1,33 @@
-# app/controllers/machines_controller.rb
 class MachinesController < ApplicationController
   before_action :authenticate_gym!
-  before_action :set_gym
   before_action :set_machine, only: [:show, :edit, :update, :destroy]
 
   def index
-    @machines = @gym.machines
+    @machines = current_gym.machines
   end
 
   def show
   end
 
   def new
-    @machine = @gym.machines.build
-  end
-
-  def edit
+    @machine = current_gym.machines.build
   end
 
   def create
-    @machine = @gym.machines.build(machine_params)
+    @machine = current_gym.machines.build(machine_creation_params)
     if @machine.save
-      redirect_to [@gym, @machine], notice: 'Machine was successfully created.'
+      redirect_to @machine, notice: 'Machine was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+  end
+
   def update
-    if @machine.update(machine_params)
-      redirect_to [@gym, @machine], notice: 'Machine was successfully updated.'
+    if @machine.update(machine_update_params)
+      redirect_to @machine, notice: 'Machine was successfully updated.'
     else
       render :edit
     end
@@ -37,24 +35,24 @@ class MachinesController < ApplicationController
 
   def destroy
     @machine.destroy
-    redirect_to gym_machines_path(@gym), notice: 'Machine was successfully destroyed.'
+    redirect_to machines_path, notice: 'Machine was successfully destroyed.'
   end
 
   private
 
-  def authenticate_gym!
-    redirect_to new_gym_session_path unless gym_signed_in?
-  end
-
-  def set_gym
-    @gym = current_gym
-  end
-
   def set_machine
-    @machine = @gym.machines.find(params[:id])
+    @machine = current_gym.machines.find(params[:id])
   end
 
-  def machine_params
-    params.require(:machine).permit(:name, :description, :compatible_exercises, :status)
+  def machine_creation_params
+    params.require(:machine).permit(:name, :description, :status).tap do |machine_params|
+      machine_params[:compatible_exercises] = params[:machine][:compatible_exercises].split(',').map(&:strip)
+    end
+  end
+
+  def machine_update_params
+    params.require(:machine).permit(:name, :description, :status).tap do |machine_params|
+      machine_params[:compatible_exercises] = params[:machine][:compatible_exercises].split(',').map(&:strip)
+    end
   end
 end
