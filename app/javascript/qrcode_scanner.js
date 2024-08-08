ae88e2b2cdae47f5c1e9d75edcda1b20302675c0
Andrre Lima <andre.salatalima@gmail.com>
2024-08-08 18:59:19 +0200

    style-QRCode-view-js-and-linkto-update

--- app/javascript/qrcode_scanner.js
+++ app/javascript/qrcode_scanner.js
@@ -1,65 +1,74 @@
-// app/javascript/qrcode_scanner.js
-
-document.addEventListener("turbo:load", function () {
+function initializeQrCodeScanner() {
   const startCameraButton = document.getElementById("start-camera");
   const reader = document.getElementById("reader");
 
-  console.log("turbo:load event triggered");
+  function startQrCodeScanner() {
+    if (window.html5QrCodeInstance) {
+      window.html5QrCodeInstance.clear().then(() => {
+        window.html5QrCodeInstance = null;
+        createQrCodeInstance();
+      }).catch(err => {});
+    } else {
+      createQrCodeInstance();
+    }
+  }
+
+  function createQrCodeInstance() {
+    const html5QrCode = new Html5Qrcode("reader");
+    html5QrCode.start(
+      { facingMode: "environment" },
+      {
+        fps: 10,
+        qrbox: { width: 250, height: 250 }
+      },
+      function onScanSuccess(decodedText, decodedResult) {
+        html5QrCode.stop().then(() => {
+          window.location.href = decodedText;
+        }).catch(err => {});
+      },
+      function onScanError(errorMessage) {}
+    ).catch(err => {});
+    window.html5QrCodeInstance = html5QrCode;
+
+    // Verificar se o vídeo está disponível e ajustá-lo
+    waitForVideoToLoad();
+  }
+
+  function waitForVideoToLoad() {
+    const video = document.querySelector('video');
+    if (video) {
+      if (video.readyState >= 2) { // Verifica se metadados estão disponíveis
+        adjustVideoElement(video);
+      } else {
+        video.addEventListener('loadedmetadata', () => {
+          adjustVideoElement(video);
+        });
+      }
+    } else {
+      // Caso o vídeo ainda não esteja no DOM, tente novamente após 100 ms
+      setTimeout(waitForVideoToLoad, 100);
+    }
+  }
+
+  function adjustVideoElement(video) {
+    video.style.position = 'absolute';
+    video.style.top = '50%';
+    video.style.left = '50%';
+    video.style.transform = 'translate(-50%, -50%)';
+    video.style.maxWidth = '100%';
+    video.style.height = 'auto';
+  }
 
   if (startCameraButton && reader) {
-    console.log("Camera button and reader elements found");
-
     startCameraButton.addEventListener("click", function () {
-      console.log("Start camera button clicked");
-
-      const script = document.createElement("script");
-      script.src = "https://cdn.jsdelivr.net/npm/html5-qrcode/minified/html5-qrcode.min.js";
-      script.onload = function () {
-        console.log("QR code library loaded");
-
-        // Adicionar um pequeno atraso para garantir que a biblioteca está carregada
-        setTimeout(() => {
-          if (typeof Html5Qrcode !== 'undefined') {
-            console.log("Html5Qrcode is defined");
-
-            const html5QrCode = new Html5Qrcode("reader");
-            console.log("Html5QrCode instance created:", html5QrCode);
-
-            html5QrCode.start(
-              { facingMode: "environment" },
-              {
-                fps: 10,
-                qrbox: { width: 250, height: 250 }
-              },
-              (decodedText, decodedResult) => {
-                console.log(`Code matched = ${decodedText}`, decodedResult);
-                // Parar a leitura do QR Code
-                html5QrCode.stop().then(() => {
-                  console.log("QR Code scanning stopped.");
-                  // Construir a URL correta
-                  const machineId = decodedText.match(/\/machines\/(\d+)\/exercises/)[1];
-                  Turbo.visit(`/machines/${machineId}/exercises`);
-                }).catch((err) => {
-                  console.error("Unable to stop scanning", err);
-                });
-              },
-              (errorMessage) => {
-                console.log(`QR Code no match. Error: ${errorMessage}`);
-              }
-            ).catch((err) => {
-              console.error("Unable to start scanning", err);
-            });
-          } else {
-            console.error("Html5Qrcode is not defined");
-          }
-        }, 1000); // Aguardar 1 segundo
-      };
-      script.onerror = function (error) {
-        console.error("Failed to load the QR code library", error);
-      };
-      document.body.appendChild(script);
+      if (typeof Html5Qrcode !== 'undefined') {
+        startQrCodeScanner();
+      } else {
+        console.error("Html5Qrcode is not defined.");
+      }
     });
-  } else {
-    console.log("Camera button or reader element not found");
   }
-});
+}
+
+document.addEventListener("turbo:render", initializeQrCodeScanner);
+document.addEventListener("turbo:load", initializeQrCodeScanner);
