fa56f3ee1c34f7aee0f31b5cb9e85208732fbf9e
Andrre Lima <andre.salatalima@gmail.com>
2024-08-23 12:22:32 +0200

    fix-stimulus-and-turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,18 +2,22 @@
 import { Turbo } from "@hotwired/turbo-rails";
 
 // Importa a aplicação Stimulus configurada no arquivo application.js dos controllers
-import { application } from "./controllers/application";
+// import { application } from "./controllers/application";
 
 // Importações adicionais para funcionalidade
 import "channels";             // Importa os canais ActionCable
 import "chartkick";            // Para uso com Chart.js
 import "Chart.bundle";         // Importa Chart.js
 
-import ChartController from "./controllers/chart_controller";
-import WeightController from "./controllers/weight_controller";
+
+// Adiciona a linha abaixo para importar todos os controladores automaticamente
+import "controllers";
+
+// import ChartController from "./controllers/chart_controller";
+// import WeightController from "./controllers/weight_controller";
 
 // Registra os controladores no objeto de aplicação do Stimulus
-application.register("chart", ChartController);
-application.register("weight", WeightController);
+// application.register("chart", ChartController);
+// application.register("weight", WeightController);
 
 Turbo.start();
