9f507b2d7c7c21d80408385a1205f386fe5dc8e0
Andrre Lima <andre.salatalima@gmail.com>
2024-10-10 11:10:55 +0200

    Select relevant sections for the new page and created PagesController

--- app/channels/arduino_data_channel.rb
+++ app/channels/arduino_data_channel.rb
@@ -1,6 +1,6 @@
 class ArduinoDataChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "arduino_data_channel"
+    stream_from "arduino_data_channel" # Subscribe to the channel
     Rails.logger.info "User connected to ArduinoDataChannel"
   end
 
@@ -8,26 +8,4 @@ class ArduinoDataChannel < ApplicationCable::Channel
     Rails.logger.info "User disconnected from ArduinoDataChannel"
   end
 
-
-  private
-
-  # def process_data(distance, timestamp)
-  #   current_time = Time.parse(timestamp)
-
-  #   if distance > @distance_threshold
-  #     if @pause_start_time.nil?
-  #       @pause_start_time = current_time
-  #     elsif current_time - @pause_start_time >= 5.seconds
-  #       # Confirma início de uma pausa
-  #       ActionCable.server.broadcast("arduino_data_channel", { event: "pause_started", time: current_time.iso8601 })
-  #       @series_start_time = nil
-  #     end
-  #   else
-  #     if @series_start_time.nil?
-  #       @series_start_time = current_time
-  #       ActionCable.server.broadcast("arduino_data_channel", { event: "series_started", time: current_time.iso8601 })
-  #     end
-  #     @pause_start_time = nil
-  #   end
-  # end
 end
--- app/models/arduino_datum.rb
+++ app/models/arduino_datum.rb
@@ -1,15 +1,15 @@
 class ArduinoDatum < ApplicationRecord
   belongs_to :exercise_set
 
-  after_create_commit :broadcast_datum
+  after_create_commit :broadcast_datum # Callback to broadcast data to the client
 
   private
 
   def broadcast_datum
     Rails.logger.info "Broadcasting data for ExerciseSet: #{exercise_set.id}"
-    arduino_data = ArduinoDatum.where(exercise_set: exercise_set).order(:recorded_at)
-    data_to_broadcast = arduino_data.map { |datum| { id: datum.id, value: datum.value, recorded_at: datum.recorded_at } }
-    ActionCable.server.broadcast "arduino_data_channel", { data: data_to_broadcast }
+    arduino_data = ArduinoDatum.where(exercise_set: exercise_set).order(:recorded_at) # Get all data for the ExerciseSet
+    data_to_broadcast = arduino_data.map { |datum| { id: datum.id, value: datum.value, recorded_at: datum.recorded_at } } # Prepare data to broadcast
+    ActionCable.server.broadcast "arduino_data_channel", { data: data_to_broadcast } # Broadcast data to the client
     Rails.logger.info "Broadcasted data: #{data_to_broadcast}"
   rescue StandardError => e
     Rails.logger.error "Error broadcasting data: #{e.message}"
