bd5a3815431bfe591c8ecac0851978d7fb5bd37a
Andrre Lima <andre.salatalima@gmail.com>
2024-10-15 09:02:18 +0200

    Updated design from relevant sections for the welcome page

--- app/channels/arduino_data_channel.rb
+++ app/channels/arduino_data_channel.rb
@@ -1,11 +1,9 @@
 class ArduinoDataChannel < ApplicationCable::Channel
   def subscribed
     stream_from "arduino_data_channel" # Subscribe to the channel
-    Rails.logger.info "User connected to ArduinoDataChannel"
   end
 
   def unsubscribed
-    Rails.logger.info "User disconnected from ArduinoDataChannel"
   end
 
 end
