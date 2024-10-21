023f8b755e0447cfe5a5348559134ac73189dd69
Andrre Lima <andre.salatalima@gmail.com>
2024-10-21 15:05:27 +0200

    Fix duplicate column issue by checking column existence

--- db/migrate/20240926115119_add_missing_columns_to_exercise_sets.rb
+++ db/migrate/20240926115119_add_missing_columns_to_exercise_sets.rb
@@ -1,15 +1,16 @@
 class AddMissingColumnsToExerciseSets < ActiveRecord::Migration[7.1]
   def change
-    add_column :exercise_sets, :reps, :integer
-    add_column :exercise_sets, :sets, :integer
-    add_column :exercise_sets, :weight, :integer
-    add_column :exercise_sets, :duration, :integer
-    add_column :exercise_sets, :rest_time, :integer
-    add_column :exercise_sets, :intensity, :string
-    add_column :exercise_sets, :feedback, :text
-    add_column :exercise_sets, :max_reps, :integer
-    add_column :exercise_sets, :effort_level, :string
-    add_column :exercise_sets, :current_series, :integer, default: 1
-    add_column :exercise_sets, :series_completed, :boolean, default: false
+    # Check if the columns exist before adding them
+    add_column :exercise_sets, :reps, :integer unless column_exists?(:exercise_sets, :reps)
+    add_column :exercise_sets, :sets, :integer unless column_exists?(:exercise_sets, :sets)
+    add_column :exercise_sets, :weight, :integer unless column_exists?(:exercise_sets, :weight)
+    add_column :exercise_sets, :duration, :integer unless column_exists?(:exercise_sets, :duration)
+    add_column :exercise_sets, :rest_time, :integer unless column_exists?(:exercise_sets, :rest_time)
+    add_column :exercise_sets, :intensity, :string unless column_exists?(:exercise_sets, :intensity)
+    add_column :exercise_sets, :feedback, :text unless column_exists?(:exercise_sets, :feedback)
+    add_column :exercise_sets, :max_reps, :integer unless column_exists?(:exercise_sets, :max_reps)
+    add_column :exercise_sets, :effort_level, :string unless column_exists?(:exercise_sets, :effort_level)
+    add_column :exercise_sets, :current_series, :integer, default: 1 unless column_exists?(:exercise_sets, :current_series)
+    add_column :exercise_sets, :series_completed, :boolean, default: false unless column_exists?(:exercise_sets, :series_completed)
   end
 end
