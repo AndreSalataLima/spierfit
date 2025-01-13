8b758fce3e1f04b8a7d0c72684b2259055d3d955
Andrre Lima <andre.salatalima@gmail.com>
2025-01-13 20:36:49 -0300

    Fix - Implemented create procotol workout for user without personal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_10_144246) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_13_224434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -209,7 +209,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_10_144246) do
     t.text "description"
     t.integer "execution_goal"
     t.bigint "user_id", null: false
-    t.bigint "personal_id", null: false
+    t.bigint "personal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "gym_id"
