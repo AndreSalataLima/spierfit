3050e7524c7a24ecf7d5966cdc56af171fafb2c0
Andrre Lima <andre.salatalima@gmail.com>
2025-02-18 17:15:22 -0300

    Fix workout protocol. Link was not related to the select_machine

--- db/migrate/20250214201526_add_workout_protocol_to_workouts.rb
+++ db/migrate/20250214201526_add_workout_protocol_to_workouts.rb
@@ -1,5 +1,5 @@
 class AddWorkoutProtocolToWorkouts < ActiveRecord::Migration[7.1]
   def change
-    add_reference :workouts, :workout_protocol, null: false, foreign_key: true
+    add_reference :workouts, :workout_protocol, null: true, foreign_key: true
   end
 end
